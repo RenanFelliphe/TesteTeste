caa23722a5cc0b5ddbfdd1b8ccd03d092607ce9b
Renan Felliphe <felliphemouraa@gmail.com>
2025-10-02 22:21:19 -0300

    "Commit que sobrou do envio faltando pelo terminal"

---
+++ explicacoes/index.js
@@ -0,0 +1,13 @@
+/*
+ < Maior que
+ > Menor que
+ <= Maior ou igual a
+ >= Maior ou igual que
+ == Igual a
+ != Diferente de
+ === Idêntico (Igual com validação de tipagem)
+ !== Não idêntico (Diferente com validação de tipagem)
+
+*/
+
+
